48ce94ea83fd3fca4060afe14ac9f9da45c95135
Harshal <saturnyx@disroot.org>
2025-07-03 20:49:37 +0800

    Refactor HTML structure for improved readability and consistency; enhance CSS styles for better layout and responsiveness

--- load.js
+++ load.js
@@ -1,17 +1,17 @@
 function clear() {
-    const loader = document.querySelector(".loader");
-    if (loader) {
-        loader.style.opacity = '0';
-        loader.style.transition = 'opacity 0.8s ease-out';
+  const loader = document.querySelector(".loader");
+  if (loader) {
+    loader.style.opacity = "0";
+    loader.style.transition = "opacity 0.8s ease-out";
 
-        setTimeout(() => {
-            loader.style.display = 'none';
-        }, 500);
-    }
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  }
 }
 
 document.onreadystatechange = () => {
-    if (document.readyState === "complete") {
-        clear();
-    }
+  if (document.readyState === "complete") {
+    clear();
+  }
 };
